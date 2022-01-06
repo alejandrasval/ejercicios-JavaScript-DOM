cff16d7fa2ee996baf53e4b1f8c4a1e784d5498e
Alejandra Sánchez <ing.alejandrasanchezv@gmail.com>
2022-01-05 23:53:03 -0500

    clock update

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,8 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
+import { digitalClock, alarm } from "./reloj.js"
 
 document.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
+  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj")
 });
 
